00335e4a3b32fdbe4e52d77bbee3f4bf27035bd0
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2020-05-09 19:23:28 +0100

    Added Upload
    Uploader doesnt work

--- app.py
+++ app.py
@@ -1,40 +1,147 @@
-from flask import Flask, render_template,request, url_for
-# from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+
+
 app = Flask(__name__)
 
-""" Below, I was gonna use SQLAlchemy instead of MySQL--  """
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# db = SQLAlchemy(app)
-""" 
-MySQL seems like it would be more expansive. I don't know if the way to create
-Models are the same. So I just commented out this block of code"""
-# class Register(db.Model):
-#     id = db.Column()
-#     username = db.Column(db.String(20), nullable=False, primary_key=True)
-#     password = db.Column(db.String(200), nullable=False)
-#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+app.secret_key = '19842hackdway'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '19842hackdway'
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+mysql = MySQL(app)
 
 
-#Home Page
-@app.route("/")
-def index():
-    user_agent = request.headers.get('User-Agent')
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    # Check if user is logged in
+    if 'loggedin' in session:
+        
+        return render_template('home.html', username=session['username'])
+    return redirect(url_for('login'))
 
-#Registration Page
-@app.route('/registration')
-def registration():
-    return render_template('registration.html')
 
-""" Login Page 
-Is this necessary? I feel like search And Login should be on the homepage
-But if the users search, and try to download without being registered, it wouldn't
-Be cool to take them back to the homepage. So... """
-@app.route('/login')
-def user(name):
-    return render_template('login.html')
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    msg=''
 
+    # Check if "username" and password" POST exist(user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Varaibles for easy access
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password) )
+        account = cursor. fetchone()
+
+        if account:
+            # Create session data, we can access this data in other routes
+
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+
+            # Redirect to home page
+            return redirect(url_for('home'))
+
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect'
+
+
+
+    
+
+
+    return render_template('login.html', msg='')
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (in form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty
+        msg = 'Please fill out the form!'
+
+    return render_template('register.html', msg=msg)
+
+
+
+@app.route('/profile')
+def profile():
+    # Check if user is logged in
+    if 'loggedin' in session:
+        # WE need all the account info
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+
+# This is for the book upload page
+@app.route('/upload')
+def upload():
+    # Check if user is logged in
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+
+        return render_template('upload.html', account = account)
+
+    # I'd like to send a 'You need to log in to upload' message, but I don't know how to do that.
+    # I'm just sending to the login page
+    return redirect(url_for('login'))
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
